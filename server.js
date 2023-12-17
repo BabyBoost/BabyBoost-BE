5246a838dc8d5ec9b5839b44245101a1eaf063f8
Abidr <abidalkhaw@gmail.com>
2023-12-17 19:02:54 +0800

    reinstall

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./src/handler/user-handler');
 const formInputHandlerRouter = require('./src/handler/formInput-handler');
 const dashboard = require('./src/handler/data-handler');
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors());
 
 app.use('/api/auth', registerRouter);
 app.use('/api/auth', loginRouter);
